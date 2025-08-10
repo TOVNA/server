e92a08efbca9ea16156777d1ed6a6998a0cce73e
bargiro1 <76563bar@gmail.com>
2025-08-11 00:12:24 +0300

    Remove the comments in the code

--- src/routes/dashboard_route.ts
+++ src/routes/dashboard_route.ts
@@ -2,9 +2,6 @@ import express from 'express';
 import {
   getGradesSummaryByStudent,
   getGradesOverTimeForStudent,
-  //getClosedQuestionsAverages
-
- 
 } from '../controllers/dashboard_controller';
 
 const router = express.Router();
@@ -33,7 +30,7 @@ const router = express.Router();
  *       200:
  *         description: List of subjects with average grades
  */
-
+router.get('/student/:studentId/grades-summary', getGradesSummaryByStudent);
 /**
  * @swagger
  * /dashboard/student/{studentId}/grades-over-time:
@@ -51,44 +48,8 @@ const router = express.Router();
  *       200:
  *         description: List of grades by date
  */
-
-/**
- * @swagger
- * /dashboard/student/{studentId}/questionnaire-summary:
- *   get:
- *     summary: Get questionnaire summary (numeric and multiple-choice) for a student
- *     tags: [Dashboard]
- *     parameters:
- *       - in: path
- *         name: studentId
- *         required: true
- *         schema:
- *           type: string
- *         description: ID of the student
- *       - in: query
- *         name: from
- *         schema:
- *           type: string
- *           format: date
- *         description: Optional start date filter (e.g. 2024-01-01)
- *       - in: query
- *         name: to
- *         schema:
- *           type: string
- *           format: date
- *         description: Optional end date filter (e.g. 2024-12-31)
- *     responses:
- *       200:
- *         description: Summary of closed question answers
- */
-
-router.get('/student/:studentId/grades-summary', getGradesSummaryByStudent);
 router.get('/student/:studentId/grades-over-time', getGradesOverTimeForStudent);
-/*
-router.get(
-  '/student/:studentId/questionnaire/:questionnaireId/closed-questions',
-  getClosedQuestionsAverages
-);
-*/
+
+
 
 export default router;
